b93615ce04144eb5109187f9429d1b11053c9d9a
Dariusz <darek200180@gmail.com>
2020-08-07 22:02:16 +0100

    Updates

---
+++ precache-manifest.5ac41a764414aabb1e9830cbc9c049f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ad743a00a7eb1a440790f64b2904165",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "c0034da5611b2e4a4d74",
+    "url": "/react-todo-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "469c74ac7945dd073f7e",
+    "url": "/react-todo-app/static/js/2.1a0d370d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-app/static/js/2.1a0d370d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0034da5611b2e4a4d74",
+    "url": "/react-todo-app/static/js/main.ee8e6e47.chunk.js"
+  },
+  {
+    "revision": "71b96540012925e35627",
+    "url": "/react-todo-app/static/js/runtime-main.3a04f730.js"
+  }
+]);
---
+++ static/js/main.ee8e6e47.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp02-todo-app"]=this["webpackJsonp02-todo-app"]||[]).push([[0],[,,,function(e,t,n){e.exports=n(10)},,,,,function(e,t,n){},function(e,t,n){},function(e,t,n){"use strict";n.r(t);var a=n(0),o=n.n(a),c=n(2),r=n.n(c);n(8),n(9);var p=function(){return o.a.createElement("div",{className:"App"},o.a.createElement("h1",null,"To do app"))};r.a.render(o.a.createElement(o.a.StrictMode,null,o.a.createElement(p,null)),document.getElementById("root"))}],[[3,1,2]]]);
+//# sourceMappingURL=main.ee8e6e47.chunk.js.map
